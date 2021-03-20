be333eb7eb77bc77280505cc2bbd5cc58ff46d66
Alex Lee <cal368@cornell.edu>
2021-03-20 17:34:53 -0400

    Refactored World Controller into Platform Controller

--- GameplayPrototype/core/src/edu/cornell/gdiac/physics/platform/PlatformController.java
+++ GameplayPrototype/core/src/edu/cornell/gdiac/physics/platform/PlatformController.java
@@ -10,6 +10,7 @@
  */
 package edu.cornell.gdiac.physics.platform;
 
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.utils.*;
 import com.badlogic.gdx.graphics.*;
@@ -20,6 +21,10 @@ import edu.cornell.gdiac.assets.AssetDirectory;
 import edu.cornell.gdiac.audio.SoundBuffer;
 import edu.cornell.gdiac.physics.*;
 import edu.cornell.gdiac.physics.obstacle.*;
+import edu.cornell.gdiac.util.PooledList;
+import edu.cornell.gdiac.util.ScreenListener;
+
+import java.util.Iterator;
 
 /**
  * Gameplay specific controller for the platformer game.  
@@ -30,11 +35,16 @@ import edu.cornell.gdiac.physics.obstacle.*;
  * This is the purpose of our AssetState variable; it ensures that multiple instances
  * place nicely with the static assets.
  */
-public class PlatformController extends WorldController implements ContactListener {
+public class PlatformController implements ContactListener, Screen {
 	///TODO: Implement a proper board and interactions between the player and chickens, slap may also be implemented here
 	////////////// This file puts together a lot of data, be sure that you do not modify something without knowing fully
 	////////////// its purpose or you may break someone else's work, further comments are below ////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/** The texture for walls and platforms */
+	protected TextureRegion earthTile;
+	/** The font for giving messages to the player */
+	protected BitmapFont displayFont;
 
 	/** Texture asset for character avatar */
 	private TextureRegion avatarTexture;
@@ -64,6 +74,30 @@ public class PlatformController extends WorldController implements ContactListen
 	/** The chicken spawn chance*/
 	private static final int SPAWN_CHANCE = 50; //1 in 50 update calls
 
+	/** The amount of time for a physics engine step. */
+	public static final float WORLD_STEP = 1/60.0f;
+	/** Number of velocity iterations for the constrain solvers */
+	public static final int WORLD_VELOC = 6;
+	/** Number of position iterations for the constrain solvers */
+	public static final int WORLD_POSIT = 2;
+
+	/** Exit code for quitting the game */
+	public static final int EXIT_QUIT = 0;
+	/** Exit code for advancing to next level */
+	public static final int EXIT_NEXT = 1;
+	/** Exit code for jumping back to previous level */
+	public static final int EXIT_PREV = 2;
+	/** How many frames after winning/losing do we continue? */
+	public static final int EXIT_COUNT = 120;
+
+	/** Width of the game world in Box2d units */
+	protected static final float DEFAULT_WIDTH  = 32.0f;
+	/** Height of the game world in Box2d units */
+	protected static final float DEFAULT_HEIGHT = 18.0f;
+	/** The default value of gravity (going down) */
+	protected static final float DEFAULT_GRAVITY = -4.9f;
+
+	/** Whether or not the player is cooking, true is they are and false otherwise*/
 	private boolean cooking;
 
 	// Physics objects for the game
@@ -84,10 +118,59 @@ public class PlatformController extends WorldController implements ContactListen
 	/** Reference to the stove object */
 	private StoveModel stove;
 
+	/** Reference to the game canvas */
+	protected GameCanvas canvas;
+	/** All the objects in the world. */
+	protected PooledList<Obstacle> objects  = new PooledList<Obstacle>();
+	/** Queue for adding objects */
+	protected PooledList<Obstacle> addQueue = new PooledList<Obstacle>();
+	/** Listener that will update the player mode when we are done */
+	private ScreenListener listener;
+
+	/** The Box2D world */
+	protected World world;
+	/** The boundary of the world */
+	protected Rectangle bounds;
+	/** The world scale */
+	protected Vector2 scale;
+
+	/** Whether or not this is an active controller */
+	private boolean active;
+	/** Whether we have completed this level */
+	private boolean complete;
+	/** Whether we have failed at this world (and need a reset) */
+	private boolean failed;
+	/** Whether or not debug mode is active */
+	private boolean debug;
+	/** Countdown active for winning or losing */
+	private int countdown;
+
 
 
 	/** Mark set to handle more sophisticated collision callbacks */
 	protected ObjectSet<Fixture> sensorFixtures;
+
+	/**
+	 * Returns true if debug mode is active.
+	 *
+	 * If true, all objects will display their physics bodies.
+	 *
+	 * @return true if debug mode is active.
+	 */
+	public boolean isDebug( ) {
+		return debug;
+	}
+
+	/**
+	 * Sets whether debug mode is active.
+	 *
+	 * If true, all objects will display their physics bodies.
+	 *
+	 * @param value whether debug mode is active.
+	 */
+	public void setDebug(boolean value) {
+		debug = value;
+	}
 
 	/**
 	 * Creates and initialize a new instance of the platformer game
@@ -95,7 +178,7 @@ public class PlatformController extends WorldController implements ContactListen
 	 * The game has default gravity and other settings
 	 */
 	public PlatformController() {
-		super(DEFAULT_WIDTH,DEFAULT_HEIGHT,DEFAULT_GRAVITY);
+		this(new Rectangle(0,0,DEFAULT_WIDTH,DEFAULT_HEIGHT), new Vector2(0,DEFAULT_GRAVITY));
 		setDebug(false);
 		setComplete(false);
 		setFailure(false);
@@ -103,6 +186,28 @@ public class PlatformController extends WorldController implements ContactListen
 		sensorFixtures = new ObjectSet<Fixture>();
 		chickens = 0;
 		cooking = false;
+	}
+
+
+	/**
+	 * Creates a new game world
+	 *
+	 * The game world is scaled so that the screen coordinates do not agree
+	 * with the Box2d coordinates.  The bounds are in terms of the Box2d
+	 * world, not the screen.
+	 *
+	 * @param bounds	The game bounds in Box2d coordinates
+	 * @param gravity	The gravitational force on this Box2d world
+	 */
+	protected PlatformController(Rectangle bounds, Vector2 gravity){
+		world = new World(gravity,false);
+		this.bounds = new Rectangle(bounds);
+		this.scale = new Vector2(1,1);
+		complete = false;
+		failed = false;
+		debug  = false;
+		active = false;
+		countdown = -1;
 	}
 
 	/**
@@ -118,14 +223,16 @@ public class PlatformController extends WorldController implements ContactListen
 		bulletTexture = new TextureRegion(directory.getEntry("platform:bullet",Texture.class));
 		chickenTexture  = new TextureRegion(directory.getEntry("platform:chicken",Texture.class));
 		stoveTexture = new TextureRegion(directory.getEntry("platform:stove",Texture.class));
+		earthTile = new TextureRegion(directory.getEntry( "shared:earth", Texture.class ));
+		displayFont = directory.getEntry( "shared:retro" ,BitmapFont.class);
 
 		jumpSound = directory.getEntry( "platform:jump", SoundBuffer.class );
 		fireSound = directory.getEntry( "platform:pew", SoundBuffer.class );
 		plopSound = directory.getEntry( "platform:plop", SoundBuffer.class );
 
 		constants = directory.getEntry( "platform:constants", JsonValue.class );
-		super.gatherAssets(directory);
 	}
+
 	
 	/**
 	 * Resets the status of the game so that we can play again.
@@ -246,7 +353,7 @@ public class PlatformController extends WorldController implements ContactListen
 	 * @return whether to process the update loop
 	 */
 	public boolean preUpdate(float dt) {
-		if (!super.preUpdate(dt)) {
+		if (!preUpdateHelper(dt)) {
 			return false;
 		}
 
@@ -268,6 +375,60 @@ public class PlatformController extends WorldController implements ContactListen
 	}
 
 	/**
+	 * Returns whether to process the update loop
+	 *
+	 * At the start of the update loop, we check if it is time
+	 * to switch to a new game mode.  If not, the update proceeds
+	 * normally.
+	 *
+	 * @param dt	Number of seconds since last animation frame
+	 *
+	 * @return whether to process the update loop
+	 */
+	private boolean preUpdateHelper(float dt){
+		InputController input = InputController.getInstance();
+		input.readInput(bounds, scale);
+		if (listener == null) {
+			return true;
+		}
+
+		// Toggle debug
+		if (input.didDebug()) {
+			debug = !debug;
+		}
+
+		// Handle resets
+		if (input.didReset()) {
+			reset();
+		}
+		if(input.didAdvance()) {
+			decrementHealth();
+		}
+		if(input.didRetreat()) {
+			killChickens();
+		}
+
+		// Now it is time to maybe switch screens.
+		if (input.didExit()) {
+			pause();
+			listener.exitScreen(this, EXIT_QUIT);
+			return false;
+		} else if (countdown > 0) {
+			countdown--;
+		} else if (countdown == 0) {
+			if (failed) {
+				reset();
+			} else if (complete) {
+				pause();
+				listener.exitScreen(this, EXIT_NEXT);
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	/**
 	 * The core gameplay loop of this world.
 	 *
 	 * This method contains the specific update code for this mini-game. It does
@@ -285,7 +446,7 @@ public class PlatformController extends WorldController implements ContactListen
 		
 		// Add a bullet if we fire
 		if (avatar.isShooting()) {
-			createBullet(InputController.getInstance().getSlapDirection());
+			createSlap(InputController.getInstance().getSlapDirection());
 		}
 		//random chance of spawning a chicken
 		if ((int)(Math.random() * (SPAWN_CHANCE + 1)) == 0) {
@@ -362,7 +523,7 @@ public class PlatformController extends WorldController implements ContactListen
 	 * Add a new bullet to the world and send it in the right direction.
 	 *
 	 */
-	private void createBullet(int direction) {
+	private void createSlap(int direction) {
 		//TODO: Instead of creating a bullet, should create a slap which behaves similarly (though inputs will be different)
 
 		///////This will require work outside of this file and method, but this is primarily where the magic happens
@@ -509,6 +670,46 @@ public class PlatformController extends WorldController implements ContactListen
 			cooking = false;
 		}
 	}
+
+	/**
+	 *
+	 * Adds a physics object in to the insertion queue.
+	 *
+	 * Objects on the queue are added just before collision processing.  We do this to
+	 * control object creation.
+	 *
+	 * param obj The object to add
+	 */
+	public void addQueuedObject(Obstacle obj) {
+		assert inBounds(obj) : "Object is not in bounds";
+		addQueue.add(obj);
+	}
+
+	/**
+	 * Immediately adds the object to the physics world
+	 *
+	 * param obj The object to add
+	 */
+	protected void addObject(Obstacle obj) {
+		assert inBounds(obj) : "Object is not in bounds";
+		objects.add(obj);
+		obj.activatePhysics(world);
+	}
+
+	/**
+	 * Returns true if the object is in bounds.
+	 *
+	 * This assertion is useful for debugging the physics.
+	 *
+	 * @param obj The object to check.
+	 *
+	 * @return true if the object is in bounds.
+	 */
+	public boolean inBounds(Obstacle obj) {
+		boolean horiz = (bounds.x <= obj.getX() && obj.getX() <= bounds.x+bounds.width);
+		boolean vert  = (bounds.y <= obj.getY() && obj.getY() <= bounds.y+bounds.height);
+		return horiz && vert;
+	}
 	
 	/** Unused ContactListener method */
 	public void postSolve(Contact contact, ContactImpulse impulse) {}
@@ -531,5 +732,290 @@ public class PlatformController extends WorldController implements ContactListen
 		if (fireSound.isPlaying( fireId )) {
 			fireSound.stop(fireId);
 		}
+	}
+
+	/**
+	 * Processes physics
+	 *
+	 * Once the update phase is over, but before we draw, we are ready to handle
+	 * physics.  The primary method is the step() method in world.  This implementation
+	 * works for all applications and should not need to be overwritten.
+	 *
+	 * @param dt	Number of seconds since last animation frame
+	 */
+	public void postUpdate(float dt) {
+		// Add any objects created by actions
+		while (!addQueue.isEmpty()) {
+			addObject(addQueue.poll());
+		}
+
+		// Turn the physics engine crank.
+		world.step(WORLD_STEP,WORLD_VELOC,WORLD_POSIT);
+
+		// Garbage collect the deleted objects.
+		// Note how we use the linked list nodes to delete O(1) in place.
+		// This is O(n) without copying.
+		Iterator<PooledList<Obstacle>.Entry> iterator = objects.entryIterator();
+		while (iterator.hasNext()) {
+			PooledList<Obstacle>.Entry entry = iterator.next();
+			Obstacle obj = entry.getValue();
+			if (obj.isRemoved()) {
+				obj.deactivatePhysics(world);
+				entry.remove();
+			} else {
+				// Note that update is called last!
+				obj.update(dt);
+			}
+		}
+	}
+
+	/**
+	 * Draw the physics objects to the canvas
+	 *
+	 * For simple worlds, this method is enough by itself.  It will need
+	 * to be overriden if the world needs fancy backgrounds or the like.
+	 *
+	 * The method draws all objects in the order that they were added.
+	 *
+	 * @param dt	Number of seconds since last animation frame
+	 */
+	public void draw(float dt) {
+		canvas.clear();
+
+		canvas.begin();
+		for(Obstacle obj : objects) {
+			obj.draw(canvas);
+		}
+		canvas.end();
+
+		if (debug) {
+			canvas.beginDebug();
+			for(Obstacle obj : objects) {
+				obj.drawDebug(canvas);
+			}
+			canvas.endDebug();
+		}
+
+		// Final message
+		if (complete && !failed) {
+			displayFont.setColor(Color.YELLOW);
+			canvas.begin(); // DO NOT SCALE
+			canvas.drawTextCentered("VICTORY!", displayFont, 0.0f);
+			canvas.end();
+		} else if (failed) {
+			displayFont.setColor(Color.RED);
+			canvas.begin(); // DO NOT SCALE
+			canvas.drawTextCentered("FAILURE!", displayFont, 0.0f);
+			canvas.end();
+		}
+	}
+
+	/**
+	 * Method to ensure that a sound asset is only played once.
+	 *
+	 * Every time you play a sound asset, it makes a new instance of that sound.
+	 * If you play the sounds to close together, you will have overlapping copies.
+	 * To prevent that, you must stop the sound before you play it again.  That
+	 * is the purpose of this method.  It stops the current instance playing (if
+	 * any) and then returns the id of the new instance for tracking.
+	 *
+	 * @param sound		The sound asset to play
+	 * @param soundId	The previously playing sound instance
+	 *
+	 * @return the new sound instance for this asset.
+	 */
+	public long playSound(SoundBuffer sound, long soundId) {
+		return playSound( sound, soundId, 1.0f );
+	}
+
+
+	/**
+	 * Method to ensure that a sound asset is only played once.
+	 *
+	 * Every time you play a sound asset, it makes a new instance of that sound.
+	 * If you play the sounds to close together, you will have overlapping copies.
+	 * To prevent that, you must stop the sound before you play it again.  That
+	 * is the purpose of this method.  It stops the current instance playing (if
+	 * any) and then returns the id of the new instance for tracking.
+	 *
+	 * @param sound		The sound asset to play
+	 * @param soundId	The previously playing sound instance
+	 * @param volume	The sound volume
+	 *
+	 * @return the new sound instance for this asset.
+	 */
+	public long playSound(SoundBuffer sound, long soundId, float volume) {
+		if (soundId != -1 && sound.isPlaying( soundId )) {
+			sound.stop( soundId );
+		}
+		return sound.play(volume);
+	}
+
+	/**
+	 * Returns true if the level is completed.
+	 *
+	 * If true, the level will advance after a countdown
+	 *
+	 * @return true if the level is completed.
+	 */
+	public boolean isComplete( ) {
+		return complete;
+	}
+
+	/**
+	 * Sets whether the level is completed.
+	 *
+	 * If true, the level will advance after a countdown
+	 *
+	 * @param value whether the level is completed.
+	 */
+	public void setComplete(boolean value) {
+		if (value) {
+			countdown = EXIT_COUNT;
+		}
+		complete = value;
+	}
+
+	/**
+	 * Returns true if the level is failed.
+	 *
+	 * If true, the level will reset after a countdown
+	 *
+	 * @return true if the level is failed.
+	 */
+	public boolean isFailure( ) {
+		return failed;
+	}
+
+	/**
+	 * Sets whether the level is failed.
+	 *
+	 * If true, the level will reset after a countdown
+	 *
+	 * @param value whether the level is failed.
+	 */
+	public void setFailure(boolean value) {
+		if (value) {
+			countdown = EXIT_COUNT;
+		}
+		failed = value;
+	}
+
+
+	/**
+	 * Returns true if this is the active screen
+	 *
+	 * @return true if this is the active screen
+	 */
+	public boolean isActive( ) {
+		return active;
+	}
+
+	/**
+	 * Returns the canvas associated with this controller
+	 *
+	 * The canvas is shared across all controllers
+	 *
+	 * @return the canvas associated with this controller
+	 */
+	public GameCanvas getCanvas() {
+		return canvas;
+	}
+
+	/**
+	 * Sets the canvas associated with this controller
+	 *
+	 * The canvas is shared across all controllers.  Setting this value will compute
+	 * the drawing scale from the canvas size.
+	 *
+	 * @param canvas the canvas associated with this controller
+	 */
+	public void setCanvas(GameCanvas canvas) {
+		this.canvas = canvas;
+		this.scale.x = canvas.getWidth()/bounds.getWidth();
+		this.scale.y = canvas.getHeight()/bounds.getHeight();
+	}
+
+	/**
+	 * Called when the Screen is resized.
+	 *
+	 * This can happen at any point during a non-paused state but will never happen
+	 * before a call to show().
+	 *
+	 * @param width  The new width in pixels
+	 * @param height The new height in pixels
+	 */
+	public void resize(int width, int height) {
+		// IGNORE FOR NOW
+	}
+
+	/**
+	 * Dispose of all (non-static) resources allocated to this mode.
+	 */
+	public void dispose() {
+		for(Obstacle obj : objects) {
+			obj.deactivatePhysics(world);
+		}
+		objects.clear();
+		addQueue.clear();
+		world.dispose();
+		objects = null;
+		addQueue = null;
+		bounds = null;
+		scale  = null;
+		world  = null;
+		canvas = null;
+	}
+
+	/**
+	 * Called when the Screen should render itself.
+	 *
+	 * We defer to the other methods update() and draw().  However, it is VERY important
+	 * that we only quit AFTER a draw.
+	 *
+	 * @param delta Number of seconds since last animation frame
+	 */
+	public void render(float delta) {
+		if (active) {
+			if (preUpdate(delta)) {
+				update(delta); // This is the one that must be defined.
+				postUpdate(delta);
+			}
+			draw(delta);
+		}
+	}
+
+	/**
+	 * Called when the Screen is resumed from a paused state.
+	 *
+	 * This is usually when it regains focus.
+	 */
+	public void resume() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Called when this screen becomes the current screen for a Game.
+	 */
+	public void show() {
+		// Useless if called in outside animation loop
+		active = true;
+	}
+
+	/**
+	 * Called when this screen is no longer the current screen for a Game.
+	 */
+	public void hide() {
+		// Useless if called in outside animation loop
+		active = false;
+	}
+
+	/**
+	 * Sets the ScreenListener for this mode
+	 *
+	 * The ScreenListener will respond to requests to quit.
+	 */
+	public void setScreenListener(ScreenListener listener) {
+		this.listener = listener;
 	}
 }
