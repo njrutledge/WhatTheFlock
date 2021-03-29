2d1c0c11fdeccd7724a41543f5e4a1dab69d5e37
Alex Lee <cal368@cornell.edu>
2021-03-29 17:14:31 -0400

    Fixed double drawing chef and minor parameter adjustments

--- GameplayPrototype/core/src/edu/cornell/gdiac/physics/platform/WorldController.java
+++ GameplayPrototype/core/src/edu/cornell/gdiac/physics/platform/WorldController.java
@@ -132,7 +132,7 @@ public class WorldController implements ContactListener, Screen {
 	/** The parameter from the list of parameters currently selected */
 	private int parameterSelected = 0;
 	/** List of all parameter values {player max health, chicken max health, base damage (player), spawn rate (per update frames), initial spawn}*/
-	private int[] parameterList = {3,5,2,50,2};
+	private int[] parameterList = {3,5,10,100,2};
 
 	/** Reference to the game canvas */
 	protected GameCanvas canvas;
