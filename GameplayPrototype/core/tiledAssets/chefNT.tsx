e02e4d4392b583591e946ddfdaa123c98b0e40d5
Alex Lee <cal368@cornell.edu>
2021-04-05 21:13:23 -0400

    Tiled stuff

---
+++ GameplayPrototype/core/tiledAssets/chefNT.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="chefNT" tilewidth="32" tileheight="32" tilecount="2360" columns="59">
+ <image source="../../../Alpha/core/assets/characters/chefNT.png" width="1911" height="1282"/>
+</tileset>
