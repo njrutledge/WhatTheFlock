643e36e04cde1b5a8e11974aeae246a813d99621
Nicholas Rutledge <njr48@cornell.edu>
2021-04-10 22:01:13 -0400

    Fixed levels being loaded mirrored

--- Alpha/core/src/code/game/controllers/GameController.java
+++ Alpha/core/src/code/game/controllers/GameController.java
@@ -47,7 +47,7 @@ public class GameController implements ContactListener, Screen {
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	//TODO: CHANGE THIS TO TEST YOUR LEVEL!
-	private final String DEFAULT_LEVEL = "TEST";
+	private final String DEFAULT_LEVEL = "level02";
 
 
 	/** The texture for walls and platforms */
