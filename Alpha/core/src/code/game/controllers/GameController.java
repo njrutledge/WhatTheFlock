4e45e307a1f652f68924ac8248802c1119151a36
Nicholas Rutledge <njr48@cornell.edu>
2021-04-13 22:25:14 -0400

    Fixed level reset on failure

--- Alpha/core/src/code/game/controllers/GameController.java
+++ Alpha/core/src/code/game/controllers/GameController.java
@@ -300,6 +300,8 @@ public class GameController implements ContactListener, Screen, InputProcessor {
 	/** Whether or not the cooldown effect is enabled */
 	private boolean cooldown;
 
+	/** Save of the current level, for resetting */
+	private JsonValue levelSave;
 
 	/** Mark set to handle more sophisticated collision callbacks */
 	protected ObjectSet<Fixture> sensorFixtures;
@@ -493,7 +495,6 @@ public class GameController implements ContactListener, Screen, InputProcessor {
 		world.setContactListener(this);
 		setComplete(false);
 		setFailure(false);
-		//populateLevel();
 	}
 	public void initEasy(){
 		parameterList = new int []{5, 100, 3, 100, 2, 6, 30, 10, 5, 5, 3, 5, 0};
@@ -515,6 +516,7 @@ public class GameController implements ContactListener, Screen, InputProcessor {
 	 */
 	public void populateLevel(JsonValue level) {
 		//TODO: Populate level similar to our board designs, and also change the win condition (may require work outside this method)\
+		levelSave = level;
 		grid.clearObstacles();
 		world.setGravity( new Vector2(0,0) );
 		volume = constants.getFloat("volume", 1.0f);
@@ -919,6 +921,7 @@ public class GameController implements ContactListener, Screen, InputProcessor {
 		} else if (countdown == 0) {
 			if (failed) {
 				reset();
+				populateLevel(levelSave);
 				return false;
 			} else if (complete) {
 				pause();
