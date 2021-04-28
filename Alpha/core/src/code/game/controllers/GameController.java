9133aa9c9db479c3a62deb134f773d2c8b7be6a7
Nicholas Rutledge <njr48@cornell.edu>
2021-04-28 11:31:36 -0400

    asset changes

--- Alpha/core/src/code/game/controllers/GameController.java
+++ Alpha/core/src/code/game/controllers/GameController.java
@@ -85,6 +85,8 @@ public class GameController implements ContactListener, Screen, InputProcessor {
 	private TextureRegion enemyHealthBarTexture;
 	/** Texture asset for trap spot*/
 	private TextureRegion trapSpotTexture;
+	/** Texture asset for toaster trap */
+	private TextureRegion trapToasterTexture;
 	/** Texture asset for the shredded chicken egg projectile */
 	private TextureRegion eggTexture;
 	/** Texture asset for the spawnpoint*/
@@ -459,6 +461,7 @@ public class GameController implements ContactListener, Screen, InputProcessor {
 			//traps
 		trapDefaultTexture = new TextureRegion(directory.getEntry("enviro:trap:spike",Texture.class));
 		trapCoolerTexture = new TextureRegion(directory.getEntry("enviro:trap:cooler",Texture.class));
+		trapToasterTexture = new TextureRegion(directory.getEntry("enviro:trap:toaster",Texture.class));
 		trapSpotTexture = new TextureRegion(directory.getEntry("enviro:trap:spot", Texture.class));
 		spawnTexture = new TextureRegion(directory.getEntry("enviro:spawn", Texture.class));
 			//characters
@@ -1322,6 +1325,9 @@ public class GameController implements ContactListener, Screen, InputProcessor {
 		switch (t){
 			case FRIDGE:
 				trapTexture = trapCoolerTexture;
+				break;
+			case BREAD_BOMB:
+				trapTexture = trapToasterTexture;
 				break;
 		}
 		float twidth = trapTexture.getRegionWidth()/scale.x;
